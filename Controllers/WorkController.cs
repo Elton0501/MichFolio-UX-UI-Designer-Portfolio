970d6af136d375554f2ef081956057215f4a46ae
ed686 <ed686@TINTIN>
2023-09-13 21:32:45 +0530

    Add project files.

---
+++ Controllers/WorkController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Michus_Portfolio.Controllers
+{
+    public class WorkController : Controller
+    {
+        // GET: Work
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult Templates()
+        {
+            return View();
+        }
+
+        public ActionResult OutOfBoxGenz()
+        {
+            return View();
+        }
+
+        public ActionResult LivingBeyond()
+        {
+            return View();
+        }
+
+        public ActionResult Vigraha()
+        {
+            return View();
+        }
+
+        public ActionResult PureCottonBags()
+        {
+            return View();
+        }
+
+        public ActionResult BSJSJ()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Michus_Portfolio.MvcApplication" Language="C#" %>
